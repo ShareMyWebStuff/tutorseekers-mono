cb4de91e8c4ed21248315a15492e1619aed5fbbe
Dave Ferguson <dave@harmonydata.co.uk>
2024-05-26 10:03:30 +0100

    Removed unused code

--- apps/frontend/src/app/(root)/page.tsx
+++ apps/frontend/src/app/(root)/page.tsx
@@ -7,23 +7,22 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 // import { getAllEvents } from '@/lib/actions/event.actions';
 import { SearchParamProps } from "@/types";
-import Image from "next/image";
-import Link from "next/link";
-import { buttonVariants } from "@/components/ui/button";
+// import Image from "next/image";
+// import Link from "next/link";
+// import { buttonVariants } from "@/components/ui/button";
 import { TutorSelector } from "@/components/landing/tutor-selector";
-import { Label } from "@/components/ui/label";
-import { Switch } from "@/components/ui/switch";
 import { OurService } from "@/components/landing/our-services";
 import { OurSubjects } from "@/components/landing/our-subjects";
 import { OurKnowledgeCenterBlogs } from "@/components/landing/knowledge-base-posts";
 import { HowItWorks } from "@/components/landing/how-it-works";
 
-export default function Home({ searchParams }: SearchParamProps) {
-  const page = Number(searchParams?.page) || 1;
-  const searchText = (searchParams?.query as string) || "";
-  const category = (searchParams?.category as string) || "";
-  const [score, setScore] = useState(0);
-  const increaseScore = () => setScore(score + 1);
+export default function Home() {
+  // export default function Home({ searchParams }: SearchParamProps) {
+  // const page = Number(searchParams?.page) || 1;
+  // const searchText = (searchParams?.query as string) || "";
+  // const category = (searchParams?.category as string) || "";
+  // const [score, setScore] = useState(0);
+  // const increaseScore = () => setScore(score + 1);
 
   //   const events = await getAllEvents({
   //     query: searchText,
