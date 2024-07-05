f58d35351e5151c48159412fa9bf090dba0a3ca1
Dave Ferguson <dave@harmonydata.co.uk>
2024-07-05 11:05:02 +0100

    Adding local GHA

--- apps/2-base-infrastructure/src/deploy-database-ddl/database-deploy-file.ts
+++ apps/2-base-infrastructure/src/deploy-database-ddl/database-deploy-file.ts
@@ -53,10 +53,16 @@ export class DatabaseDeployFile {
       const response = await s3.getObject(s3Params).promise();
       console.log("response");
       console.log(response);
-      const fileContent = response.Body?.toString("utf-8");
+      const fileContent = response.Body?.toString("utf-8").replace(
+        new RegExp("\r", "g"),
+        "",
+      );
       console.log("fileContent");
       console.log(fileContent);
-      const tmpLines: string[] = !fileContent ? [] : fileContent?.split("\r\n");
+      const tmpLines: string[] = !fileContent ? [] : fileContent?.split("\n");
+
+      console.log("Line Count");
+      console.log(tmpLines.length);
 
       // Remove comments and blank lines
       const lines = tmpLines.filter(
