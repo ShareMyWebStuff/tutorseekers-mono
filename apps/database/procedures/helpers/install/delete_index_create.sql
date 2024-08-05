7b267b181fbf753b2f176c4cac7e283216c37056
Dave Ferguson <dave@harmonydata.co.uk>
2024-08-05 10:00:42 +0100

    Working on registration complete

--- apps/database/procedures/helpers/install/delete_index_create.sql
+++ apps/database/procedures/helpers/install/delete_index_create.sql
@@ -5,12 +5,22 @@ CREATE PROCEDURE DeleteIndex
 )
 BEGIN
 
-    IF EXISTS ( SELECT * INTO IndexExists
-        FROM    INFORMATION_SCHEMA.STATISTICS
-        WHERE   table_schema = database()
-        AND     table_name   = tablename
-        AND     index_name   = indexname;
-    ) THEN
-        ALTER TABLE  tablename DROP index indexname;
+    DECLARE IndexExists INTEGER;
+
+    SELECT  COUNT(1) INTO IndexExists
+    FROM    INFORMATION_SCHEMA.STATISTICS
+    WHERE   table_schema = database()
+    AND     table_name   = tablename
+    AND     index_name   = indexname;
+
+    IF IndexExists > 0 THEN
+        SET @sqlstmt = CONCAT('DROP', uniqueFlag,  ' INDEX ',indexname,' ON ',
+        database(),'.',tablename,' (',columns,')');
+        PREPARE st FROM @sqlstmt;
+        EXECUTE st;
+        DEALLOCATE PREPARE st;
+        SELECT CONCAT('Created index ',indexname,' on table ', tablename) CreateIndexRes;
+
     END IF;
+        
 END
